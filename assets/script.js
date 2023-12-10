37959f8cbc716ac43cc3ec9b8b2d62cf40356bc2
rshaarma <rohittsharrma@outlook.com>
2023-12-10 01:17:19 +0000

    added logic in scriptjs file

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+init();
+function init() {
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+  timeRows();
+  setInterval(timeRows, 50000);
+  $(".time-block").each(function () {
+    var id = $(this).attr("id");
+    $("#" + id + " textarea").text(
+      localStorage.getItem(dayjs().format("dddd, MMMM D") + id)
+    );
+  });
+  $(".saveBtn").on("click", handleSave);
+}
+
+function timeRows() {
+  $(".time-block").each(function () {
+    var timeRowHour = parseInt($(this).attr("id").replace("hour-", ""));
+    var currentHour = parseInt(dayjs().format("H"));
+    $(this).removeClass("past present future");
+    if (timeRowHour < currentHour) {
+      $(this).addClass("past");
+    } else if (timeRowHour > currentHour) {
+      $(this).addClass("future");
+    } else {
+      $(this).addClass("present");
+    }
+  });
+}
+function handleSave(event) {
+  var hourId = $(this).parent().attr("id");
+  localStorage.setItem(
+    dayjs().format("DDD YYYY") + hourId,
+    $("#" + hourId + " textarea").val()
+  );
+}
